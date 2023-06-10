95a5704b119bc4e8835e5f1c8235cbead91f053e
Riyas Valiyadan <riyas.vmc@gmail.com>
2023-06-10 10:46:22 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import static java.lang.Thread.sleep;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        sleep(4000);
+        System.out.println("Hello world!");
+    }
+}
